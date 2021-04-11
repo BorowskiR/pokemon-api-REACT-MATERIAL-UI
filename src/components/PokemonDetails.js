069287b0c31d18db326e0e8f2c586d7dcfad7121
Rafal Borowski <rafall.borowski@gmail.com>
2021-04-11 21:20:19 +0200

    Taking params id from history props

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function Pokemon() {
-  return <div>pokemon details</div>;
+function Pokemon(props) {
+  const { match } = props;
+  const { params } = match;
+  const { id } = params;
+
+  return <div>{`this is the pokemon page for pokemon #${id}`}</div>;
 }
 
 export default Pokemon;
