470e803857d5b61e625f076ea2abd47b5818660c
Rafal Borowski <rafall.borowski@gmail.com>
2021-04-11 22:46:06 +0200

    Add basic card

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import { AppBar, Toolbar, Grid } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  pokedexContainer: {
+    padding: '20px 50px',
+  },
+});
+
+const getPokemonCard = () => {
+  return (
+    <Grid item xs={12} sm={4}>
+      <Card>
+        <CardContent></CardContent>
+      </Card>
+    </Grid>
+  );
+};
 
 function Pokedex() {
+  const classes = useStyles();
   return (
     <>
       <AppBar position="static">
         <Toolbar />
       </AppBar>
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          item1
-        </Grid>
-        <Grid item xs={4}>
-          item2
-        </Grid>
+      <Grid container spacing={1} className={classes.pokedexContainer}>
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
       </Grid>
     </>
   );
