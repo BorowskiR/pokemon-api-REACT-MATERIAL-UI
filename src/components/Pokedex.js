e3dd33664fa939537804f8dece9a3487274117f9
Rafal Borowski <rafall.borowski@gmail.com>
2021-04-11 21:50:42 +0200

    Add basic layout

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { AppBar, Toolbar, Grid } from '@material-ui/core';
 
 function Pokedex() {
-  return <div>pokedex</div>;
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar />
+      </AppBar>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          item1
+        </Grid>
+        <Grid item xs={4}>
+          item2
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default Pokedex;
