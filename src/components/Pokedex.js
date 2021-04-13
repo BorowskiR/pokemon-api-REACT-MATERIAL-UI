627df5dce29c157949eef8af4c55e5928837167f
Rafal Borowski <rafall.borowski@gmail.com>
2021-04-13 23:17:00 +0200

    Filter pokedex for specific pokemon

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -8,13 +8,15 @@ import {
   CardContent,
   CircularProgress,
   Typography,
+  TextField,
 } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, fade } from '@material-ui/core/styles';
 import mockData from '../mockData';
 import { capitalizeFirstLetter } from '../helpers/capitalizeName';
 import axios from 'axios';
+import SearchIcon from '@material-ui/icons/Search';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   pokedexContainer: {
     padding: '20px 50px',
   },
@@ -24,11 +26,29 @@ const useStyles = makeStyles({
   cardContent: {
     textAlign: 'center',
   },
-});
+  searchContainer: {
+    display: 'flex',
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    marginTop: '5px',
+    marginBottom: '5px',
+  },
+  searchIcon: {
+    alignSelf: 'flex-end',
+    marginBottom: '5px',
+  },
+  searchInput: {
+    width: '200px',
+    margin: '5px',
+  },
+}));
 
 function Pokedex(props) {
   const [pokemonData, setPokemonData] = useState(mockData);
   const { history } = props;
+  const [filter, setFilter] = useState('');
+  const classes = useStyles();
 
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon?limit=50`).then((res) => {
@@ -51,12 +71,13 @@ function Pokedex(props) {
     });
   }, []);
 
-  const classes = useStyles();
+  const handleChange = (e) => {
+    setFilter(e.target.value);
+  };
 
-  const getPokemonCard = (pokemonId) => {
+  const createPokemonCard = (pokemonId) => {
     const { id, name, sprite } = pokemonData[pokemonId];
 
-    console.log(pokemonData[`${id}`]);
     return (
       <Grid item xs={12} sm={4} key={id}>
         <Card onClick={() => history.push(`/${pokemonId}`)}>
@@ -76,11 +97,25 @@ function Pokedex(props) {
   return (
     <>
       <AppBar position="static">
-        <Toolbar />
+        <Toolbar>
+          <div className={classes.searchContainer}>
+            <SearchIcon className={classes.searchIcon} />
+            <TextField
+              label="Pokemon"
+              className={classes.searchInput}
+              onChange={handleChange}
+            />
+          </div>
+        </Toolbar>
       </AppBar>
       {pokemonData ? (
         <Grid container spacing={1} className={classes.pokedexContainer}>
-          {Object.keys(pokemonData).map((pokemon) => getPokemonCard(pokemon))}
+          {Object.keys(pokemonData).map((pokemonId) => {
+            return (
+              pokemonData[pokemonId].name.includes(filter) &&
+              createPokemonCard(pokemonId)
+            );
+          })}
         </Grid>
       ) : (
         <CircularProgress />
